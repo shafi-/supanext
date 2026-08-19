750c53555d461f0052625177b8fad618b6259e5b
shafi- <shafi.cse.buet@gmail.com>
2026-08-19 17:48:17 +0600

    fix: exclude e2e tests from vitest

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['tests/e2e/**', 'node_modules/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
